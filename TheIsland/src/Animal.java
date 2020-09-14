55eef63e0bbc84bebefa3de8241f2c9f77466b27
Zachary Siefkes <zacharysiefkes@gmail.com>
2020-09-14 17:09:28 +1200

    Initial commit

---
+++ TheIsland/src/Animal.java
@@ -0,0 +1,15 @@
+
+public interface Animal {
+//	- move
+	public void move();
+//	- detect food
+//	public void detectFood();
+////	- detect water
+//	public void detectWater();
+////	- detect predators
+//	public void detectPredators();
+////	- detect prey
+//	public void detectPrey();
+////	- mate with creatures of the same type
+//	public void mate();
+}
---
+++ TheIsland/src/Rabbit.java
@@ -0,0 +1,4 @@
+
+public class Rabbit implements Animal {
+
+}
